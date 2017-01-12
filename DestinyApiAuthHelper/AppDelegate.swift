c834c430c0f35cddcd14ca1bdcede1be5fc2d0e9
Grayson <info@fromconcentratesoftware.com>
2017-01-11 20:27:33 -0500

    Initial Commit

---
+++ DestinyApiAuthHelper/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DestinyApiAuthHelper
+//
+//  Created by Grayson Hansard on 1/11/17.
+//  Copyright © 2017 From Concentrate Software. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
---
+++ DestinyApiAuthHelper/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DestinyApiAuthHelper
+//
+//  Created by Grayson Hansard on 1/11/17.
+//  Copyright © 2017 From Concentrate Software. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
